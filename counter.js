a9aeeefefc70157741cdb320d9afb570437df890
phoenixtype <usakuma@uwaterloo.ca>
2022-03-27 19:07:07 -0400

    Scoreboard complete

--- counter.js
+++ counter.js
@@ -1,29 +1,60 @@
 // set inital value to zero
-let count = 0;
+let countA = 0;
+let countB = 0;
 // select value and buttons
-const value = document.querySelector("#value");
+const valueA = document.querySelector("#valueA");
+const valueB = document.querySelector("#valueB");
+
 const btns = document.querySelectorAll(".btn");
 
 btns.forEach(function (btn) {
   btn.addEventListener("click", function (e) {
     const styles = e.currentTarget.classList;
-    if (styles.contains("decrease")) {
-      count--;
-    } else if (styles.contains("increase")) {
-      count++;
+    if (styles.contains("decreaseA")) {
+      countA--;
+    } else if (styles.contains("increaseA")) {
+      countA++;
+    } else if (styles.contains("resetA")) {
+      countA = 0;
     } else {
-      count = 0;
+      return true;
     }
 
-    if (count > 0) {
-      value.style.color = "green";
+    if (countA > 0) {
+      valueA.style.color = "green";
     }
-    if (count < 0) {
-      value.style.color = "red";
+    if (countA < 0) {
+      valueA.style.color = "red";
     }
-    if (count === 0) {
-      value.style.color = "#222";
+    if (countA === 0) {
+      valueA.style.color = "#222";
     }
-    value.textContent = count;
+    valueA.textContent = countA;
+  });
+});
+
+btns.forEach(function (btn) {
+  btn.addEventListener("click", function (e) {
+    const styles = e.currentTarget.classList;
+    if (styles.contains("decreaseB")) {
+      countB--;
+    } else if (styles.contains("increaseB")) {
+      countB++;
+    } else if (styles.contains("resetB")) {
+      countB = 0;
+    } else {
+      return true;
+    }
+
+    if (countB > 0) {
+      valueB.style.color = "green";
+    }
+    if (countB < 0) {
+      valueB.style.color = "red";
+    }
+    if (countB === 0) {
+      valueB.style.color = "#222";
+    }
+    valueB.textContent = countB;
   });
 });
